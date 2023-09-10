c555e9880023d7209776994c03cededf90b5cae1
SimerLol <rootsimer@gmail.com>
2023-09-10 21:36:12 +1000

    fixed: ReferenceError: db is not defined

--- Projects/AuthX/middleware/authenticated.js
+++ Projects/AuthX/middleware/authenticated.js
@@ -1,11 +1,15 @@
+const path = require("path")
 const jwt = require('jsonwebtoken');
-const JWT_SECRET_KEY = 'CybryX'; // Replace this with your actual secret key
+const JWT_SECRET_KEY = 'REDACTED'; // Replace this with your actual secret key
+
+// Database Configurations
+const db = require(path.join(__dirname, '/../../', 'db.json'));
 
 // Middleware to check if the user is authenticated (using the JWT token)
 function isAuthenticated(req, res, next) {
   // Get the token from the request header or body
-  const token = req.cookies || req.header('Authorization') || req.body.token || '';
-
+  const token = req.cookies.token || req.header('Authorization') || req.body.token || '';
+  
   // Check if the token is present
   if (!token) {
     return res.status(401).json({ error: 'Missing token' });
